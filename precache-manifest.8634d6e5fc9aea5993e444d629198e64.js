8f8e49c9e8a4c6db85653b365637865d68bdaca0
aargee123 <ram.gumber1456@gmail.com>
2020-03-01 02:22:35 +0530

    Updates

---
+++ precache-manifest.8634d6e5fc9aea5993e444d629198e64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef12cdb95bbe60598e48524475d8574",
+    "url": "/RobotFriends/index.html"
+  },
+  {
+    "revision": "91b37df2a0d958d617bb",
+    "url": "/RobotFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "72bad0679a0399fed223",
+    "url": "/RobotFriends/static/css/main.cd60a2a9.chunk.css"
+  },
+  {
+    "revision": "91b37df2a0d958d617bb",
+    "url": "/RobotFriends/static/js/2.ae4971c4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/RobotFriends/static/js/2.ae4971c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72bad0679a0399fed223",
+    "url": "/RobotFriends/static/js/main.404c4e9b.chunk.js"
+  },
+  {
+    "revision": "901ac984bdcdc5bdcc60",
+    "url": "/RobotFriends/static/js/runtime-main.f0e75398.js"
+  }
+]);
